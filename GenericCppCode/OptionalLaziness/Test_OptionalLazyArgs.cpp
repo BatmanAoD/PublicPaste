559a56966f98a150e579242d1a403a9400de5e9e
Kyle Strand <kyle.strand@beckman.com>
2016-07-05 15:34:56 -0600

    Added a C++ header for 'optionally lazy' arguments

---
+++ GenericCppCode/OptionalLaziness/Test_OptionalLazyArgs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "OptionalLazyArgs.hpp"
+
+void PermitLazy(
+    LAZY_PARAM(int) my_int)
+{
+  std::cout << "Called 'PermitLazy'." << std::endl;
+  std::cout << "Got possibly-lazy int: " << my_int << std::endl;
+}
+
+int ExpensivelyGenerateInt(void)
+{
+  std::cerr << " <[( Generating int (pretend this call is expensive)! )]> ";
+  std::cerr.flush();
+  return 7;
+}
+
+int main(void)
+{
+  PermitLazy(LAZY_ARG(ExpensivelyGenerateInt()));
+  PermitLazy(EAGER_ARG(ExpensivelyGenerateInt()));
+  PermitLazy(EAGER_ARG(3));
+  // See comment above `EAGER_ARG`
+  // PermitLazy(ExpensivelyGenerateInt());
+}
